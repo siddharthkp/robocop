d8232ae2996f6f0497d94be4dabd04e8d425ecfc
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2015-07-22 02:37:36 +0530

    Removed tasks matched in response

--- src/hook.js
+++ src/hook.js
@@ -5,11 +5,8 @@ app.post('/hook', function (req, res) {
     var tasksMatched = [];
     for (var i = 0; i < robocop.tasks.length; i++) {
         task = robocop.tasks[i];
-        if (task.condition(payload)) {
-            var action = task.method(payload);
-            if (action) tasksMatched.push(task.name);
-        }
+        if (task.condition(payload)) task.method(payload);
     }
-    res.end(tasksMatched.length + ' task' + (tasksMatched > 1 ? 's' : '') + ' matched. ' + tasksMatched.join(', '));
+    res.end('ok');
 });
 
