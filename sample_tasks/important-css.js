344e1c3eb77ce306abc45cfe8a113dc35cf46e32
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2015-08-12 13:07:00 +0000

    Cleaned up some parts of !important task

--- sample_tasks/important-css.js
+++ sample_tasks/important-css.js
@@ -2,6 +2,7 @@ var method = function(payload) {
     var pullRequest = payload.pull_request;
     robocop.helpers.diff(pullRequest, function(files) {
         var filename, extension, changes;
+        var commit_id = payload.pull_request.head.sha;
         for (var i = 0; i < files.length; i++) {
             filename = files[i].name;
             extension = filename.substr(filename.lastIndexOf('.'));
@@ -9,15 +10,9 @@ var method = function(payload) {
                 // TODO: Crappy code to check diff. Move this to helper
                 changes = files[i].changes;
                 for (var j = 0; j < changes.length; j++) {
-                    if (changes[j][0] === '+') {
+                    if (isAddition(changes[j])) {
                         if (changes[j].indexOf('!important') !== -1) {
-                            robocop.helpers.comment('review', {
-                                pullRequest: pullRequest,
-                                body: 'Not on my watch!. Check out [the docs](https://github.com/practo/practo/blob/master/docs/css-styleguide.md#specificity).',
-                                commit_id: payload.pull_request.head.sha,
-                                path: filename,
-                                position: j
-                            });
+                          comment(pullRequest, commit_id, filename, j);
                         }
                     }
                 }
@@ -25,6 +20,20 @@ var method = function(payload) {
         }
     });
 };
+
+function isAddition(line) {
+    return line[0] === '+';
+}
+
+function comment(pullRequest, commit_id, filename, position) {
+    robocop.helpers.comment('review', {
+        pullRequest: pullRequest,
+        body: 'Not on my watch!. Check out [the docs](https://github.com/practo/practo/blob/master/docs/css-styleguide.md#specificity).',
+        commit_id: commit_id,
+        path: filename,
+        position: position
+    });
+}
 
 var condition = function(payload) {
     if (payload.event_type === 'pull_request' && ['opened', 'reopened', 'synchronize'].indexOf(payload.action) !== -1) {
