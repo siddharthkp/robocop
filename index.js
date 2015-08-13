f46954a077b322e534bf9d1ebb5c5980b5299872
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2015-08-13 06:31:44 +0000

    Caching tasks from repo folder on startup

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ var express = require('express');
 GLOBAL.app = express();
 var bodyParser = require('body-parser');
 GLOBAL.config = require('./config.json');
-var server = app.listen(config.port, function () {
-      console.log('My friends call me Murphy. You call me ...');
-      console.log('Robocop.');
-});
-app.use(bodyParser.json());
-
 GLOBAL.robocop = require('./src/robocop');
+
+console.log('My friends call me Murphy. You call me ...');
+console.log('Robocop.');
+
 require('require-all')(__dirname + '/src/helpers');
-require('require-all')(__dirname + '/' + config.tasks);
-require('./src/home');
-require('./src/hook');
+
+console.log();
+console.log('Caching tasks');
+robocop.helpers.cacheTasks(function () {
+    require('require-all')(__dirname + '/cached_tasks');
+    require('./src/home');
+    require('./src/hook');
+    startServer();
+});
+
+function startServer() {
+    var server = app.listen(config.port, function () {
+        console.log('My duty begins. Somewhere there is a crime happening.');
+    });
+    app.use(bodyParser.json());
+}
 
---
+++ src/helpers/cache-tasks.js
@@ -0,0 +1,88 @@
+var request = require('request');
+var fs = require('fs');
+
+var tasksFound = 0;
+var globalCallback;
+
+var cacheTasks = function(callback) {
+    globalCallback = callback;
+    getRepoTree(function(tree){
+        filterTaskFiles(tree, function(taskFiles) {
+            downloadFiles(taskFiles);
+        })
+    });
+}
+
+function getRepoTree(callback) {
+    var url = 'https://api.github.com/repos/';
+    url += config.repo_owner + '/';
+    url += config.repo_name + '/';
+    url += 'git/trees/adding-robocop-tasks';
+    url += '?recursive=1';
+    url += '&access_token=' + config.github_token;
+    var options = {
+        url: url,
+        headers: {
+            'User-Agent': 'robocop'
+        }
+    };
+
+    request(options, function(error, response, body) {
+        var data = JSON.parse(body);
+        if (data.truncated) {
+            console.log('Some tasks might be missed! Please report as a bug.');
+        }
+        var tree = data.tree;
+        callback(tree);
+    });
+}
+
+function filterTaskFiles(tree, callback) {
+    var directory = config.tasks_directory;
+    var taskFiles = [];
+    for (var i = 0; i < tree.length; i++) {
+        if (tree[i].path.lastIndexOf(directory + '/', 0) === 0) {
+            taskFiles.push({
+                name: tree[i].path.replace(directory + '/', ''),
+                url: tree[i].url
+            });
+        }
+    }
+    tasksFound = taskFiles.length;
+    console.log('Caching ' + tasksFound + ' tasks');
+    callback(taskFiles);
+}
+
+function downloadFiles(taskFiles) {
+    for (var i = 0; i < taskFiles.length; i++) {
+        downloadFile(taskFiles[i].name, taskFiles[i].url);
+    }
+}
+
+function downloadFile(name, url) {
+    var options = {
+        url: url + '?access_token=' + config.github_token,
+        headers: {
+            'User-Agent': 'robcop',
+        }
+    };
+
+    request(options, function(error, response, body) {
+        var data = JSON.parse(body);
+        var contents = new Buffer(data.content, 'base64').toString('ascii');
+        fs.writeFile('cached_tasks/' + name, contents);
+        incrementTasksCounter();
+    });
+}
+
+var tasksCached = 0;
+function incrementTasksCounter() {
+    tasksCached++;
+    if (tasksCached === tasksFound) {
+        console.log(tasksCached + ' tasks cached');
+        globalCallback();
+    }
+}
+
+robocop.helpers.cacheTasks = cacheTasks;
+
