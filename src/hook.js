fc7a64f98088b73393bface2612176b1474138fa
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-06-04 11:52:29 +0530

    Removed unused tasksMatched

--- src/hook.js
+++ src/hook.js
@@ -2,7 +2,6 @@ app.post('/hook', function (req, res) {
     var payload = req.body;
     payload.event_type = req.headers['x-github-event'];
     var task;
-    var tasksMatched = [];
     for (var i = 0; i < robocop.tasks.length; i++) {
         task = robocop.tasks[i];
         if (task.condition(payload)) task.method(payload);
