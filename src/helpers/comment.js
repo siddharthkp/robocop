37f0cef4a687ace031cb8a9aba9aff5e1ad89cf3
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-06-04 12:08:29 +0530

    Cleaned up comment helper

--- src/helpers/comment.js
+++ src/helpers/comment.js
@@ -1,31 +1,3 @@
-var comment = function(type, data) {
-    if (config.debug) {
-        console.log(type);
-        console.log(data);
-    }
-    else if (type === 'commit') commitComment(data);
-    else if (type === 'review') reviewComment(data);
-};
-
-robocop.helpers.comment = comment;
-
-var commitComment = function(data) {
-    var commit = data.commit;
-    var message = data.message;
-    var url = commit.url.replace('github.com/', 'api.github.com/repos/').replace('commit', 'commits') + '/comments?access_token=' + config.github_token;
-    var data = {
-        body: message
-    }
-    post(url, data);
-};
-
-var reviewComment = function(data) {
-    var pullRequest = data.pullRequest;
-    var url = pullRequest.url + '/comments?access_token=' + config.github_token;
-    delete data.pullRequest;
-    post(url, data);
-};
-
 function post(url, data, callback) {
     var request = require('request');
     var options = {
@@ -40,6 +12,38 @@ function post(url, data, callback) {
 
     request(options, function(error, response, body) {
         console.log(response.statusCode, url);
-        if (callback) callback(response.statusCode, body);
+        if (callback) return callback(response.statusCode, body);
     });
 }
+
+var commitComment = function(data) {
+    var commit = data.commit;
+    var message = data.message;
+    var url = commit.url;
+    url = url.replace('github.com/', 'api.github.com/repos/');
+    url = url.replace('commit', 'commits');
+    url = url + '/comments?access_token=' + config.github_token;
+    var postData = {
+        body: message
+    };
+    post(url, postData);
+};
+
+var reviewComment = function(data) {
+    var pullRequest = data.pullRequest;
+    var url = pullRequest.url + '/comments?access_token=' + config.github_token;
+    delete data.pullRequest;
+    post(url, data);
+};
+
+var comment = function(type, data) {
+    if (config.debug) {
+        console.log(type);
+        console.log(data);
+    }
+    else if (type === 'commit') commitComment(data);
+    else if (type === 'review') reviewComment(data);
+};
+
+robocop.helpers.comment = comment;
+
