4de49255bbf6a0bdb347a9959077986bbaea7282
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-06-04 12:17:29 +0530

    Cleaned up important-css

--- sample_tasks/important-css.js
+++ sample_tasks/important-css.js
@@ -1,14 +1,3 @@
-var method = function(payload) {
-    var pullRequest = payload.pull_request;
-    robocop.helpers.diff(pullRequest, function(files) {
-        var commit_id = payload.pull_request.head.sha;
-        // sniff - files, filetype, word, function
-        robocop.helpers.sniff(files, 'css', '!important', function(filename, j) {
-            comment(pullRequest, commit_id, filename, j);
-        });
-    });
-};
-
 function comment(pullRequest, commit_id, filename, position) {
     robocop.helpers.comment('review', {
         pullRequest: pullRequest,
@@ -23,6 +12,17 @@ var condition = function(payload) {
     if (payload.event_type === 'pull_request' && ['opened', 'reopened', 'synchronize'].indexOf(payload.action) !== -1) {
         return true;
     }
+};
+
+var method = function(payload) {
+    var pullRequest = payload.pull_request;
+    robocop.helpers.diff(pullRequest, function(files) {
+        var commit_id = payload.pull_request.head.sha;
+        // sniff - files, filetype, word, function
+        robocop.helpers.sniff(files, 'css', '!important', function(filename, j) {
+            comment(pullRequest, commit_id, filename, j);
+        });
+    });
 };
 
 robocop.helpers.register('!important', condition, method);
