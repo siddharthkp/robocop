b6765adfa1e5d1e78e7d59ca61334bce1b27867a
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-06-04 12:15:19 +0530

    Cleaned up newrelic.js

--- newrelic.js
+++ newrelic.js
@@ -1,24 +1,14 @@
-/**
- * New Relic agent configuration.
- *
- * See lib/config.defaults.js in the agent distribution for a more complete
- * description of configuration variables and their potential values.
- */
+// New Relic agent configuration.
 exports.config = {
-  /**
-   * Array of application names.
-   */
-  app_name: 'robocop',
-  /**
-   * Your New Relic license key.
-   */
-  license_key: config.newrelic_key,
-  logging: {
-    /**
-     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
-     * issues with the agent, 'info' and higher will impose the least overhead on
-     * production applications.
-     */
-    level: 'info'
-  }
-}
+    app_name: 'robocop',
+    license_key: config.newrelic_key,
+    logging: {
+        /**
+         * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+         * issues with the agent, 'info' and higher will impose the least overhead on
+         * production applications.
+         */
+        level: 'info'
+    }
+};
+
